e0c31c6e4899fab104353685c091947d5e27f355
Tejas1331 <77189865+Tejas1331@users.noreply.github.com>
2025-04-23 21:26:03 +0530

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -39,8 +39,8 @@ def get_latest_data():
     df = pd.DataFrame(parsed_data, columns=['timestamp', 'actual_price', 'predicted_price'])
 
     # Shift predicted_price to t+2
-    df['predicted_price'] = df['predicted_price'].shift(2)
-    df['predicted_timestamp'] = df['timestamp'].shift(-2)
+    df['predicted_price'] = df['predicted_price'].shift(0)
+    df['predicted_timestamp'] = df['timestamp'].shift(0)
 
     
     return df.tail(120)
