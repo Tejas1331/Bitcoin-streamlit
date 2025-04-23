b2831612f8b39f64fa4d22dd57d611b4e40aa41f
Tejas1331 <77189865+Tejas1331@users.noreply.github.com>
2025-04-23 21:30:09 +0530

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -57,7 +57,7 @@ while True:
 
         fig, ax = plt.subplots(figsize=(10, 4))
         ax.plot(df['timestamp'], df['actual_price'], label="Actual Price", color='blue', linewidth=2)
-        ax.plot(df['predicted_timestamp'], df['predicted_price'], label="Predicted Price (t+2)",  color='orange', marker='+', linestyle='None', markersize=2)
+        ax.plot(df['predicted_timestamp'], df['predicted_price'], label="Predicted Price (t+2)",  color='red', marker='+', linestyle='None', markersize=2)
 
         ax.set_xlabel("Timestamp")
         ax.set_ylabel("Bitcoin Price")
